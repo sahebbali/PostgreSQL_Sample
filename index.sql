ae9a75ba57b9edf932076e27d5f8865f61586d17
sahebbali <saheb1996jmr@gmail.com>
2024-12-21 15:13:42 +0600

    add CRUD operation

--- index.sql
+++ index.sql
@@ -7,3 +7,18 @@ CREATE TABLE employees (
     salary DECIMAL(10,2) DEFAULT 30000.00,
     hire_date DATE NOT NULL DEFAULT CURRENT_DATE
 );
+-- // INSERT Data
+INSERT INTO employees (fname, lname, email, dept, salary, hire_date)
+VALUES ('John', 'Doe', 'john.doe@example.com', 'IT', 50000.00, '2024-12-21');
+-- update Data
+UPDATE employees
+SET salary = 55000.00, dept = 'HR'
+WHERE emp_id = 1;
+
+-- get Data
+SELECT * FROM employees;
+SELECT * FROM employees WHERE emp_id = 1;
+SELECT * FROM employees WHERE dept = 'IT';
+
+-- delete recored
+DELETE FROM employees WHERE emp_id = 1;
