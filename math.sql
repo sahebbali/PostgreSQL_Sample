92adb3319a7aeedb56b331fc06271ae682c5d607
sahebbali <saheb1996jmr@gmail.com>
2024-12-21 16:41:49 +0600

    add group by

--- math.sql
+++ math.sql
@@ -15,3 +15,9 @@ salary = (SELECT MAX(salary) FROM employees)
 
 SELECT SUM(salary) FROM employees;
 SELECT AVG(salary) FROM employees
+
+-- GROUP By
+-- Groups the rows based on the unique values in the dept column.
+SELECT dept FROM employees GROUP BY dept;
+SELECT dept, COUNT(fname) FROM employees GROUP
+BY dept
